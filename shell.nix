70920767daf715340c5f55f914f7ec0987d33994
Lukas Hermansson <lukas@lukashermansson.me>
2024-06-19 23:59:16 +0200

    add nix shell config

---
+++ shell.nix
@@ -0,0 +1,26 @@
+let
+  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+  rustVersion = "latest";
+  #rustVersion = "1.62.0";
+  rust = pkgs.rust-bin.nightly.${rustVersion}.default.override {
+    extensions = [
+      "rust-src" # for rust-analyzer
+      "rust-analyzer"
+    ];
+    targets = [ "wasm32-unknown-unknown" ];
+  };
+in
+pkgs.mkShell {
+  buildInputs = [
+    rust
+  ] ++ (with pkgs; [
+    pkg-config
+    cargo-leptos
+    tailwindcss
+    # other dependencies
+    #gtk3
+    #wrapGAppsHook
+  ]);
+  RUST_BACKTRACE = 1;
+}
